66afe548cd1e01e3ae61d25230c3adf49baf2e17
Refiloe Radebe <52778663+kweenDev@users.noreply.github.com>
2024-06-02 01:58:57 +0200

    Update settings.py
    Remove key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pqj3pawzojv9rtbm-z01uk%ci21nwn@oy2@jsw7#!blfm9!&kq'
+SECRET_KEY = 'django-insecure' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
